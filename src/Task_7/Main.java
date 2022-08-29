b6095ef0ed98c92e11fab3f62c7ed0ff2a647b2d
VitaliyKlochko <89vklochko89@gmail.com>
2022-08-29 18:38:56 +0300

    Begin7

---
+++ src/Task_7/Main.java
@@ -0,0 +1,17 @@
+package Task_7;
+
+public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
+    public static void main(String[] args) {
+
+        // Task 1.2 / 7
+        System.out.println();
+        System.out.println(ANSI_YELLOW + "1.2 Переменные /  Задача 7" + ANSI_RESET);
+        System.out.println();
+
+    }
+}
